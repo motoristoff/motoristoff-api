aada539c0dd5bf27c59da86af3993e6dda70682c
Vladislav <ungefuk.vlad@mail.ru>
2026-09-24 18:13:44 +0300

    backend

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 transporter.verify((error, success) => {
